f602a60a2810c0b7b8cad7e6f46f4d0625bcadbd
sszsfan <sszsfan@gmail.com>
2016-04-14 17:05:57 +0800

    4.14update
    4.14update

--- js/my.js
+++ js/my.js
@@ -1,7 +1,7 @@
 function showWrapper(){
 	var wraps = document.getElementsByClassName("js_wrapper");
 	var masks = document.getElementsByClassName("js_mask");
-	var btns = document.getElementsByClassName("js_purchase_methods");
+	var btns = document.getElementsByClassName("js_control_btn001");
 	var closes = document.getElementsByClassName("js_close_wrap");
 	var ypos = -240;
 	var timer1,timer2;
@@ -9,7 +9,7 @@ function showWrapper(){
 		//masks[0].style.visibility = "visible";
 		//masks[0].style.opacity = 1;
 		masks[0].setAttribute("class","js_mask mask_2");
-		moveElement("js_wrap",0,3);
+		wraps[0].setAttribute("class","js_wrapper wrapper_2");
 		/*
 		function moveUp(){
 			if(ypos < 0){
@@ -26,7 +26,7 @@ function showWrapper(){
 		//masks[0].style.visibility = "hidden";
 		//masks[0].style.opacity = 0;
 		masks[0].setAttribute("class","js_mask mask_1");
-		moveElement("js_wrap",-260,3);
+		wraps[0].setAttribute("class","js_wrapper wrapper_1");
 		/*
 		function moveDown(){
 			if(ypos>-240){
@@ -41,60 +41,8 @@ function showWrapper(){
 	};
 	masks[0].onclick = function(){
 		masks[0].setAttribute("class","js_mask mask_1");
-		moveElement("js_wrap",-260,3);
+    wraps[0].setAttribute("class","js_wrapper wrapper_1");
 	};
-}
-/*
-function moveElement(elementID,final_y,interval) {
-  if (!document.getElementById) return false;
-  if (!document.getElementById(elementID)) return false;
-  var elem = document.getElementById(elementID);
-  if (elem.movement) {
-    clearTimeout(elem.movement);
-  }
-  if (!elem.style.bottom) {
-    elem.style.bottom = "-260px";
-  }
-  var ypos = parseInt(elem.style.bottom);
-  if (ypos == final_y) {
-    return true;
-  }
-  if (ypos < final_y) {
-    var dist = Math.ceil((final_y - ypos)/10);
-    ypos = ypos + dist;
-  }
-  if (ypos > final_y) {
-    var dist = Math.ceil((ypos - final_y)/10);
-    ypos = ypos - dist;
-  }
-  elem.style.bottom = ypos + "px";
-  var repeat = "moveElement('"+elementID+"',"+final_y+","+interval+")";
-  elem.movement = setTimeout(repeat,interval);
-}
-*/
-function moveElement(elementID,final_y,interval) {
-  if (!document.getElementById) return false;
-  if (!document.getElementById(elementID)) return false;
-  var elem = document.getElementById(elementID);
-  if (elem.movement) {
-    clearTimeout(elem.movement);
-  }
-  if (!elem.style.bottom) {
-    elem.style.bottom = "-260px";
-  }
-  var ypos = parseInt(elem.style.bottom);
-  if (ypos == final_y) {
-    return true;
-  }
-  if (ypos < final_y) {
-    ypos = ypos + 5;
-  }
-  if (ypos > final_y) {
-    ypos = ypos - 5;
-  }
-  elem.style.bottom = ypos + "px";
-  var repeat = "moveElement('"+elementID+"',"+final_y+","+interval+")";
-  elem.movement = setTimeout(repeat,interval);
 }
 
 window.onload=showWrapper;
